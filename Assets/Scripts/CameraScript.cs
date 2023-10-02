7aeb1257c5999cdb232fa9a58a65adf3f7e19277
Joel Holgerson <joel.holgerson@learnet.se>
2023-10-02 16:22:46 +0200

    Trying to fix camera

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,6 +14,6 @@ public class Camera : MonoBehaviour
     void Update()
     {
         GameObject camera = GameObject.FindWithTag("Player");
-        transform.position = camera.transform.position + new Vector3(0,0,-10);
+        transform.position = Vector3.MoveTowards(transform.position, camera.transform.position, 0.1f);
     }
 }
